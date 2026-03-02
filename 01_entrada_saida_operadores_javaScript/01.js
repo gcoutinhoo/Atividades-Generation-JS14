4a2fab1b1af60bf85db8413ffcb9ccb96617e59f
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-03-02 13:32:18 -0300

    feat: atividade 1

---
+++ 01_entrada_saida_operadores_javaScript/01.js
@@ -0,0 +1,12 @@
+const leia = require('readline-sync');
+
+let salario;
+let abono;
+let novoSalario;
+
+salario = leia.questionFloat('Digite o salario do funcionario: ');
+abono = leia.questionFloat('Digite o valor do abono: ');
+
+novoSalario = salario + abono;
+
+console.log(`O novo salario do funcionario é: ${novoSalario}`);
