9d8b13e2c273f64b4635edb9dedeada5d38869cf
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-03-03 13:08:09 -0300

    Iniciando atividade 2 'com erros'

---
+++ atividades02_03-03/atividadeif02.js
@@ -0,0 +1,15 @@
+import leia from "prompt-sync";
+
+let num;
+
+num = leia.questionInt("\nDigite um número: ");
+
+if(num % 2 == 0 && num >= 0){
+    console.log(`\nO número ${num} é par e positivo` );
+}else if(num % 2 == 0 && num <= 0){
+    console.log(`\nO número ${num} é par e negativo` );
+}else if(num % 2 != 0 && num >= 0){
+    console.log(`\nO número ${num} é ímpar e positivo`)
+}else{
+    console.log(`\nO número ${num} é ímpar e negativo`)
+}
