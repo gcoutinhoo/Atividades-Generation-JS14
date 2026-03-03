7e74165c559846946b227eac4f86690d47c1060a
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-03-03 13:50:05 -0300

    Fix: Corrigindo erro, alteração de sync readline-sync

--- atividades02_03-03/atividadeif02.js
+++ atividades02_03-03/atividadeif02.js
@@ -1,15 +1,15 @@
-import leia from "prompt-sync";
+import leia from "readline-sync";
 
 let num;
 
 num = leia.questionInt("\nDigite um número: ");
 
 if(num % 2 == 0 && num >= 0){
-    console.log(`\nO número ${num} é par e positivo` );
+    console.log(`\nO número ${num} é par e positivo!` );
 }else if(num % 2 == 0 && num <= 0){
-    console.log(`\nO número ${num} é par e negativo` );
+    console.log(`\nO número ${num} é par e negativo!` );
 }else if(num % 2 != 0 && num >= 0){
-    console.log(`\nO número ${num} é ímpar e positivo`)
+    console.log(`\nO número ${num} é ímpar e positivo!`)
 }else{
-    console.log(`\nO número ${num} é ímpar e negativo`)
+    console.log(`\nO número ${num} é ímpar e negativo!`)
 }
