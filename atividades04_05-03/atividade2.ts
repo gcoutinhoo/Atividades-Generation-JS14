86f140edc38ee527d6e5126f0d5465eec5d0e607
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-03-05 13:44:39 -0300

    Feat: Adicionando atividade 2, utilizei a base da atividade de vetores de ontem pra fazer, gostei

---
+++ atividades04_05-03/atividade2.ts
@@ -0,0 +1,15 @@
+import readlineSync from "readline-sync";
+
+const numero: Array<number> = [2, 5, 1, 3, 4, 9, 7, 8, 10, 6];
+let encontrado: boolean = false;
+let loc = readlineSync.questionInt(`Digite o número que você deseja encontrar: `);
+
+for (let i = 0; i < numero.length; i++) {
+        if(numero[i] == loc){
+            encontrado = true;
+            console.log(`O número ${numero[i]} está localizado na posição ${i}`);
+        }
+}
+if (encontrado == false){
+    console.log(`O número ${loc} não foi encontrado!`)
+}
