4d16403c4e2012018e426446c32af7712f5f2970
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-03-05 14:51:37 -0300

    Fix: Organizando comentarios para nao esquecer

--- atividades04_05-03/atividade4.ts
+++ atividades04_05-03/atividade4.ts
@@ -1,15 +1,15 @@
 import readlineSync from "readline-sync";
 
 const numero = new Set<number>([2, 5, 1, 3, 4, 9, 7, 8, 10, 6]);
-let encontrado: boolean = false;
+let encontrado: boolean = false; // variavel para verificar se o numero foi encontrado ou não
 let loc = readlineSync.questionInt(`Digite o número que você deseja encontrar: `);
 
 for (let num of numero){
         if (num == loc){
             encontrado = true;
-            console.log(`O número ${loc} foi encontrado!`);
+            console.log(`O número ${loc} foi encontrado!`); // se o numero for encontrado, exibe a mensagem
         }
 }
-if (encontrado == false){
+if (encontrado == false){ // se o numero não for encontrado, exibe a mensagem
 console.log(`O número ${loc} não foi encontrado!`)
 }
