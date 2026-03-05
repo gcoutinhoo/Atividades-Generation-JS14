16dc14b9db59df165a8e53598fcf3a6b63dc3040
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-03-05 13:24:05 -0300

    Feat: Iniciando TypeScript, atividade 1

---
+++ atividades04_05-03/atividade1.ts
@@ -0,0 +1,13 @@
+import readlineSync from "readline-sync";
+
+const cores: Array<string> = new Array<string>(5);
+
+for(let i = 0; i < cores.length ; i++){
+    cores[i] = readlineSync.question("Digite suas cores: ")
+}
+
+console.log(`Listar todas as cores:\n${cores}`);
+
+cores.sort();
+
+console.log(`Ordenar as cores:\n${cores}`);
