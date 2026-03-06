d53523158f6d7c2006b891c1d7d513d5e2906559
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-03-06 12:33:41 -0300

    Feat: Iniciando atividade pilha

--- atividades05_06-03/atividade-pilha.ts
+++ atividades05_06-03/atividade-pilha.ts
@@ -1,6 +1,18 @@
 import { Stack } from "./Stack.ts";
+import * as readlineSync from "readline-sync";
 
-const pilha = new Stack<number>();
-pilha.push(6);
+const pilha = new Stack<string>();
+let opcao: number = 0;
 
-pilha.printStack();
+console.log("===========================================\n");
+console.log("      1 - Adicionar Livro na Pilha");
+console.log("      2 - Listar todos os Livros");
+console.log("      3 - Retirar Livro da Pilha");
+console.log("      0 - Sar");
+console.log("\n===========================================\n");
+
+opcao = readlineSync.questionInt("Digite uma opção: ");
+
+do {
+    
+} while (opcao !== 0);
